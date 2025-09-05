95429bf7042678bb511515b03f9cb59dc09fa042
Slawa Pidgorny <depidsvy@nintendo.de>
2025-09-05 14:19:22 +0200

    upgrade node and tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,85 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	return (
+		<main className="grid grid-cols-3 max-w-7xl mx-auto px-3 py-3 gap-3 text-black">
+			<div className="bg-lime-500 rounded p-3">
+				<div className="flex justify-between">
+					<h1 className="text-3xl mb-3">
+						<span>The Formula</span>
+					</h1>
+					<span><Link className="underline"
+											href="https://d11n7da8rpqbjy.cloudfront.net/basharet/1832_1708988558001Formula.pdf?kuid=04106a28-b725-4166-8f6a-14986af68a9f&kref=dHfjznGvImXc">Details</Link></span>
+				</div>
+				<p className="mb-3">Bashar's follow your excitement formula is the key to being your True Self and living your
+					dreams!</p>
+				<ol className="my-3 list-decimal list-inside space-y-3">
+					<li>
+						Act on your excitement, your passion, whatever is most exciting to you, in the moment. Do this every moment
+						that you can.
+					</li>
+					<li>
+						Do this to the best of your ability. Take it as far as you can go until you cannot take it any further.
+					</li>
+					<li>
+						Act on your excitement/passion with absolutely no insistence, assumption or expectation of what the outcome
+						should be.
+					</li>
+					<li>
+						Choose to remain in a positive state regardless of what happens.
+					</li>
+					<li>
+						Constantly investigate your belief systems. Release & replace the un-preferred beliefs: fear-based beliefs,
+						and the beliefs not in alignment with who you prefer to be.
+					</li>
+				</ol>
+			</div>
+			<div className="bg-pink-500 rounded p-3">
+				<h1 className="text-3xl mb3">
+					The Five Laws of Creation
+				</h1>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+				<ol className="my-3 list-decimal list-inside space-y-3">
+					<li>
+						You exist
+					</li>
+					<li>
+						Everything is here and now
+					</li>
+					<li>
+						The One is the All and the All are the One
+					</li>
+					<li>
+						What you put out is what you get back
+					</li>
+					<li>
+						Everything changes, except the first 4 Laws
+					</li>
+				</ol>
+			</div>
+			<div className="bg-sky-500 rounded p-3">
+				<h1 className="text-3xl mb-3">Sacred Circuitry</h1>
+				<p className="mb-3">Directions: Look at each one for 1 minute (In order from left to right, top to bottom) and
+					allow yourself to feel the different states of each symbol, feel the activation of these states, observe and
+					absorb each symbol and the concept attached as you go through them. It only takes 15 minutes to reprogram your
+					brain to be more sensitive to higher vibrational energy and function as a more loving, holistic,
+					multidimensional being with a broader view than just the physical. The brain is like plastic and can be
+					rewired/remolded after only 15 minutes. You are this flexible and this powerful!
+				</p>
+				<p>
+					Do this for 3 days in a row (in order for 1 min each) for a stronger and deeper initial impact and after that
+					you can go out of order or just focus on 1 or a few symbols you are attracted to. If you wish to still go in
+					order every time, that works. Always keep it to 15 minutes, even if you are using a few symbols and you can do
+					it up to 3 times per day. Do not go over 15 minutes or over 3x/day as the brain needs time to fully absorb and
+					process the info, any more may burn you out or just waste time. </p>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+				<div>
+					https://d11n7da8rpqbjy.cloudfront.net/basharet/1847_1708989106891Sacred_Circuitry.pdf?kuid=04106a28-b725-4166-8f6a-14986af68a9f&kref=dHfjznGvImXc
+				</div>
+				<div>
+					https://www.pinterest.com/pin/709528116296260473/
+				</div>
+			</div>
+		</main>
+	);
 }
